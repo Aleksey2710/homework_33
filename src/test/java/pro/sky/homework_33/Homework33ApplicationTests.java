1ac32ac77b82f485c85b181cabb8eea266b114af
Aleksey2710 <Aleksey_2710@mail.ru>
2023-03-03 09:22:39 +0300

    initial commit

---
+++ src/test/java/pro/sky/homework_33/Homework33ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
